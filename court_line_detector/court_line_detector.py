092dae359e3b46b474a208188f211f7b57c08a05
Vitor Pietrobom <vitorpietrobom@gmail.com>
2024-04-03 14:29:04 -0300

    feat: Court detection done

--- court_line_detector/court_line_detector.py
+++ court_line_detector/court_line_detector.py
@@ -6,9 +6,10 @@ import cv2
 
 class CourtLineDetector:
     def __init__(self, model_path):
-        self.model = models.resnet50(pretrained=False)
+        self.model = models.resnet50(pretrained=True)
         self.model.fc = torch.nn.Linear(self.model.fc.in_features, 28)
-        self.model.load_state_dict(torch.load(model_path, map_location="cpu"))
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.load_state_dict(torch.load(model_path, map_location=device))
 
         self.transform = transforms.Compose([
             transforms.ToPILImage(),
@@ -17,6 +18,8 @@ class CourtLineDetector:
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
+
+    #Running only in the first frame because the court doesnt change position in the video
     def predict(self,image):
         img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image_tensor = self.transform(img_rgb).unsqueeze(0)
@@ -24,4 +27,32 @@ class CourtLineDetector:
         with torch.no_grad():
             outputs = self.model(image_tensor)
 
-        keypoints = outputs.squeeze().tolist()
+        keypoints = outputs.squeeze().cpu().numpy()
+        # These unsqueeze and squeeze are used to remove the image from a list and then add it back
+        original_height, original_width = img_rgb.shape[:2]
+
+        keypoints[::2] *= original_width/224.0
+        keypoints[1::2] *= original_height/224.0
+
+        return keypoints
+    
+    def draw_keypoints(self, image, keypoints):
+        for i in range(0, len(keypoints), 2):
+            x = int(keypoints[i])
+            y = int(keypoints[i+1])
+
+            cv2.putText(image, str(i//2), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.circle(image, (x, y), 5, (0, 255, 0), -1)
+
+        return image
+    
+    def draw_keypoints_on_video(self, video_frames, keypoints):
+        output_frames = []
+
+        for frame in video_frames:
+            frame= self.draw_keypoints(frame, keypoints)
+            output_frames.append(frame)
+
+        return output_frames
+
+
